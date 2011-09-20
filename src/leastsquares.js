5400229f50787de21a20f6407ef561edd88e24cc
Tim Lovorn <tflovorn@crimson.ua.edu>
2011-09-20 01:16:31 -0500

    .5 replaced by 0.5 (JSLint complained about .5)

--- src/leastsquares.js
+++ src/leastsquares.js
@@ -15,14 +15,14 @@ function Line(leftY, rightY, color) {
 // color = color of grid lines
 Raphael.fn.drawGrid = function (x, y, w, h, wv, hv, color) {
     color = color || "#000";
-    var path = ["M", Math.round(x) + .5, Math.round(y) + .5, "L", Math.round(x + w) + .5, Math.round(y) + .5, Math.round(x + w) + .5, Math.round(y + h) + .5, Math.round(x) + .5, Math.round(y + h) + .5, Math.round(x) + .5, Math.round(y) + .5],
+    var path = ["M", Math.round(x) + 0.5, Math.round(y) + 0.5, "L", Math.round(x + w) + 0.5, Math.round(y) + 0.5, Math.round(x + w) + 0.5, Math.round(y + h) +0.5, Math.round(x) + 0.5, Math.round(y + h) + 0.5, Math.round(x) + 0.5, Math.round(y) + 0.5],
         rowHeight = h / hv,
         columnWidth = w / wv;
     for (var i = 1; i < hv; i++) {
-        path = path.concat(["M", Math.round(x) + .5, Math.round(y + i * rowHeight) + .5, "H", Math.round(x + w) + .5]);
+        path = path.concat(["M", Math.round(x) + 0.5, Math.round(y + i * rowHeight) + 0.5, "H", Math.round(x + w) + 0.5]);
     }
     for (i = 1; i < wv; i++) {
-        path = path.concat(["M", Math.round(x + i * columnWidth) + .5, Math.round(y) + .5, "V", Math.round(y + h) + .5]);
+        path = path.concat(["M", Math.round(x + i * columnWidth) + 0.5, Math.round(y) + 0.5, "V", Math.round(y + h) + 0.5]);
     }
     return this.path(path.join(",")).attr({"stroke": color});
 };
@@ -42,7 +42,7 @@ Raphael.fn.drawLines = function (lines, leftX, rightX, convertY) {
             rightY = convertY(rightYCoord);
         // Construct the SVG path string.
         var pathList = ["M", leftX, leftY, "L", rightX, rightY],
-            path = this.path(pathList.join(",")).attr({"stroke": color, "stroke-width": 5});
+            path = this.path(pathList.join(",")).attr({"stroke": color, "stroke-width":0.5});
         paths = paths.concat(path);
     }
     return paths;
@@ -74,7 +74,7 @@ window.onload = function() {
     // Index of lineColors to use for color of the next line created
     var lineColorIndex = 1;
     // temp. data
-    var dataX = [1, 2, 3, 4, 5],
+    var dataX = [1, 2, 3, 4,0.5],
         dataY = [2, 4, 6, 8, 10];
     // Find the extrema of the data to calculate scaling
     var minX = Math.min.apply(null, dataX),
@@ -89,15 +89,15 @@ window.onload = function() {
     // Create the Rapael context inside div element "holder"
     var r = Raphael("holder", width, height);
     // Draw the grid (for reading off coordinates)
-    var gridX = leftgutter + .5,
-        gridY = topgutter + .5,
+    var gridX = leftgutter + 0.5,
+        gridY = topgutter + 0.5,
         gridWidth = width - leftgutter - rightgutter,
         gridHeight = height - topgutter - bottomgutter;
     r.drawGrid(gridX, gridY, gridWidth, gridHeight, 10, 10, "#333");
     // Closure over the grid variables to convert a coordinate on the grid to 
     // pixel space.  For passing to r.drawLines().
     var coordToPixelsY = function(y) {
-        return gridY + y * scaleY;
+        return gridY + gridHeight - y * scaleY;
     }
     // Initialize the line list with one line.
     var lines = [new Line(meanY, meanY, lineColors[0])];
